1fa4ba4ccc449b877cb73d95ca7f481b3ac793f9
viktorkh <viktorkh@gmail.com>
2016-01-23 20:09:36 +0200

    23/01/16

--- ParkMe_WepApi/Controllers/OrderController.cs
+++ ParkMe_WepApi/Controllers/OrderController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ParkMe_WepApi.DAL;
+using System.Data.Entity.Core.Objects;
+using System.Globalization;
 
 namespace ParkMe_WepApi.Controllers
 {
@@ -26,6 +29,59 @@ namespace ParkMe_WepApi.Controllers
         public bool ConfirmTaskById([FromBody]string token, [FromBody]string orderId, [FromBody]string taskId)
         {
             throw new Exception();
+        }
+        [HttpPost]
+        //   public string SetOrder([FromBody]string token, [FromBody]string orderTime, [FromBody]string orderAddress)
+        public string SetOrder(string token, string orderTime, string orderAddress)
+
+        {
+
+
+            //using (TestDBEntities1 context = new TestDBEntities1())
+            //{
+            //  //  context.spCreateOrder(token, DateTime.Parse(orderTime), orderAddress);
+            //}
+
+
+            return "fff";
+        }
+
+
+        [HttpGet]
+        //   public string SetOrder([FromBody]string token, [FromBody]string orderTime, [FromBody]string orderAddress)
+        public string SetOrderGet(string token, string orderTime, string orderAddress)
+
+        {
+            string result="";
+            int? d;
+            try
+            {
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                string format = "dd-MM-yyyy HH:mm";
+                DateTime time = DateTime.ParseExact(orderTime,format,provider);
+
+                using (TestDBEntities1 context = new TestDBEntities1())
+                {
+                    //  result = context.spCreateOrder(token, time, orderAddress,returnVal:d).ToString();
+                 var dd= context.spCreateOrder1(token, time, orderAddress).FirstOrDefault();
+
+
+                    result = dd.ToString(); 
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+
+            return result;
+        }
+
+        public string Post()
+        {
+
+            return "POST Hello";
         }
     }
 }
